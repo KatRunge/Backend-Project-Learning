3926b39ad7784cb82dfc06f61fd833534ebbeacf
KatRunge <katja.runge@jimdo.com>
2025-10-23 10:57:26 +0200

    feat: react app initial setup with typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+
+export const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Forms to create new employees or addresses</h1>
+        <div className="counter-section">
+          <div className="button-group"></div>
+        </div>
+      </header>
+    </div>
+  );
+};
